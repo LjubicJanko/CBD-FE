9c73fbdd9645e619fbc62bc2afe72cdc69a06374
ljubicjanko1@gmail.com <ljubicjanko1@gmail.com>
2024-11-08 00:11:34 +0100

    after build

--- src/pages/signup/Signup.page.tsx
+++ src/pages/signup/Signup.page.tsx
@@ -1,28 +1,29 @@
-import { ErrorMessage, Field, Form, Formik } from "formik";
-import { useContext } from "react";
-import AuthContext from "../../store/AuthProvider/Auth.context";
-import { RegisterData } from "../../types/Auth";
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+// import { useContext } from 'react';
+// import AuthContext from '../../store/AuthProvider/Auth.context';
+import { RegisterData } from '../../types/Auth';
 
 const SignupPage = () => {
-  const { signup } = useContext(AuthContext);
+  // const { signup } = useContext(AuthContext);
 
   return (
     <div>
       signup page
       <Formik
         initialValues={
-          { username: "", password: "", fullName: "" } as RegisterData
+          { username: '', password: '', fullName: '' } as RegisterData
         }
         validate={(values) => {
           const errors: { username?: string } = {};
           if (!values.username) {
-            errors.username = "Required";
+            errors.username = 'Required';
           }
           return errors;
         }}
         onSubmit={(values, { setSubmitting }) => {
-          const res = signup(values);
-          console.log(res);
+          console.log(values);
+          // const res = signup(values);
+          // console.log(res);
           setSubmitting(false);
         }}
       >
