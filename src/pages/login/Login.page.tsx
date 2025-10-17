50e055ddafc5c307da4e72e90d215024a246acfc
Janko Ljubic <ljubicjanko1@gmail.com>
2025-10-17 23:13:11 +0200

    update login page

--- src/pages/login/Login.page.tsx
+++ src/pages/login/Login.page.tsx
@@ -1,3 +1,5 @@
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import {
   Button,
   CircularProgress,
@@ -8,14 +10,13 @@ import {
 } from '@mui/material';
 import { useFormik } from 'formik';
 import { useContext, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 import AuthContext from '../../store/AuthProvider/Auth.context';
 import { LoginData } from '../../types/Auth';
-import * as Styled from './Login.styles';
-import { useNavigate } from 'react-router-dom';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import { useTranslation } from 'react-i18next';
 import { textInputSX } from '../../util/util';
+import * as Styled from './Login.styles';
+
 
 const initialValues: LoginData = {
   username: '',
@@ -59,42 +60,53 @@ const LoginComponent = () => {
         </div>
       )}
       <form autoComplete="off" onSubmit={formik.handleSubmit}>
+        <h1 className="login-container__title">{t('CBD SPORTSWEAR')}</h1>
         <div className="fields">
-          <TextField
-            variant="outlined"
-            color="primary"
-            type="text"
-            name="username"
-            placeholder={t('username')}
-            fullWidth
-            value={formik.values.username}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={formik.touched.username && Boolean(formik.errors.username)}
-            helperText={formik.touched.username && formik.errors.username}
-          />
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={showPassword ? 'text' : 'password'}
-            name="password"
-            placeholder={t('password')}
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            sx={textInputSX}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  onMouseUp={handleMouseUpPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
+          <div className="login-container__username">
+            <label id="username-label">{t('Korisnicko ime')}</label>
+            <TextField
+              aria-labelledby="username-label"
+              variant="outlined"
+              color="primary"
+              type="text"
+              name="username"
+              placeholder={t('username')}
+              className="login-container__username__field"
+              fullWidth
+              value={formik.values.username}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.username && Boolean(formik.errors.username)}
+              helperText={formik.touched.username && formik.errors.username}
+            />
+          </div>
+          <div className="login-container__password">
+            <label id="password-label">{t('Lozinka')}</label>
+            <OutlinedInput
+              aria-labelledby="password-label"
+              id="outlined-adornment-password"
+              className="login-container__password__field"
+              type={showPassword ? 'text' : 'password'}
+              name="password"
+              placeholder={t('password')}
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              sx={textInputSX}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    onMouseUp={handleMouseUpPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </div>
         </div>
         <Button
           variant="contained"
--- src/pages/login/Login.styles.tsx
+++ src/pages/login/Login.styles.tsx
@@ -16,7 +16,55 @@ export const LoginContainer = styled.div`
     padding-top: 32px;
   `)}
 
+  .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline {
+    color: ${theme.PRIMARY_2};
+    border-color: ${theme.PRIMARY_2} !important;
+    border-width: 2px;
+    border-radius: 25px;
+    -webkit-text-fill-color: ${theme.PRIMARY_2};
+    caret-color: ${theme.PRIMARY_2};
+  }
+
+  .MuiInputBase-input {
+    padding: 8px;
+    caret-color: ${theme.PRIMARY_2};
+    color: ${theme.PRIMARY_2};
+    &:-webkit-autofill {
+      -webkit-box-shadow: 0 0 0 1000px ${theme.PRIMARY_1} inset;
+      -webkit-text-fill-color: ${theme.PRIMARY_2};
+      caret-color: ${theme.PRIMARY_2};
+      transition: background-color 5000s ease-in-out 0s;
+    }
+  }
+
+  label {
+    color: ${theme.PRIMARY_2};
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 25px;
+    margin-left: 8px;
+  }
+
   .login-container {
+    &__username,
+    &__password {
+      display: flex;
+      flex-direction: column;
+      .MuiInputBase-root {
+        border-radius: 25px;
+        background-color: ${theme.PRIMARY_1};
+      }
+    }
+
+    &__title {
+      font-weight: 400;
+      font-size: 32px;
+      line-height: 25px;
+      text-align: center;
+      vertical-align: middle;
+      color: ${theme.PRIMARY_2};
+    }
+
     &__loader-wrapper {
       position: fixed;
       top: 0;
@@ -29,6 +77,21 @@ export const LoginContainer = styled.div`
       align-items: center;
       z-index: 9999;
     }
+
+    &__instagram {
+      color: ${theme.PRIMARY_2};
+      position: absolute;
+      bottom: 36px;
+      left: 50%;
+      right: 50%;
+      gap: 4px;
+    }
+  }
+
+  .login-container__password {
+    .MuiButtonBase-root {
+      color: ${theme.PRIMARY_2};
+    }
   }
 
   form {
@@ -36,24 +99,22 @@ export const LoginContainer = styled.div`
     flex-direction: column;
     width: fit-content;
     gap: 32px;
-    background-color: #ffffff1f;
-    padding: 40px;
+    background-color: ${theme.PRIMARY_1};
     border-radius: 20px;
+    border: 2px solid ${theme.PRIMARY_2};
+    padding: 48px 100px;
+    width: 607px;
+    height: 424px;
 
     ${tablet(css`
       padding: 32px;
     `)}
 
-    .MuiInputBase-root {
-      color: white;
-      border-color: white;
-    }
-
     .fields {
       display: flex;
       flex-direction: column;
-      width: fit-content;
-      gap: 8px;
+      gap: 30px;
+      width: 100%;
     }
 
     .login-btn {
