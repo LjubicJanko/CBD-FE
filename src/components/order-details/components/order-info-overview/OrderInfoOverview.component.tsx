cc8c6fe3c13c81ec869c4379804e45741e437477
Janko Ljubic <ljubicjanko1@gmail.com>
2026-02-22 09:52:52 +0100

    display note and description to manufacturer

--- src/components/order-details/components/order-info-overview/OrderInfoOverview.component.tsx
+++ src/components/order-details/components/order-info-overview/OrderInfoOverview.component.tsx
@@ -1,159 +1,176 @@
-import { ReactNode, useContext, useMemo } from 'react';
+import { ReactNode, useMemo } from 'react';
 import { Rating, Table, TableBody, TableRow } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import useResponsiveWidth from '../../../../hooks/useResponsiveWidth';
 import {
-  Order,
-  OrderExecutionStatusEnum,
-  orderPriorityArray,
+    Order,
+    OrderExecutionStatusEnum,
+    orderPriorityArray,
 } from '../../../../types/Order';
 import { xxsMax } from '../../../../util/breakpoints';
 import * as Styled from './OrderInfoOverview.styles';
 import classNames from 'classnames';
-import AuthContext from '../../../../store/AuthProvider/Auth.context';
 import theme from '../../../../styles/theme';
 
 export type OrderInfoOverviewProps = {
-  selectedOrder?: Order;
+    selectedOrder?: Order;
 };
 
 type OrderInfoConfigType = {
-  label: string;
-  value: string | ReactNode | undefined;
+    label: string;
+    value: string | ReactNode | undefined;
 };
 
 const OrderInfoOverview = ({ selectedOrder }: OrderInfoOverviewProps) => {
-  const { t } = useTranslation();
-  const width = useResponsiveWidth();
-  const { authData } = useContext(AuthContext);
-  const isMobile = width < xxsMax;
-  const isPaused =
-    selectedOrder?.executionStatus === OrderExecutionStatusEnum.PAUSED;
+    const { t } = useTranslation();
+    const width = useResponsiveWidth();
+    const isMobile = width < xxsMax;
+    const isPaused =
+        selectedOrder?.executionStatus === OrderExecutionStatusEnum.PAUSED;
 
-  const orderInfoConfig: OrderInfoConfigType[] = useMemo(
-    () =>
-      [
-        { label: t('order-name'), value: selectedOrder?.name },
-        authData?.roles?.includes('admin') && {
-          label: t('description'),
-          value: selectedOrder?.description,
-        },
-        { label: t('note'), value: selectedOrder?.note },
-        { label: t('acquisition-cost'), value: selectedOrder?.acquisitionCost },
-        { label: t('sale-price'), value: selectedOrder?.salePrice },
-        ...(selectedOrder?.legalEntity
-          ? [
-              {
-                label: t('sale-price-taxed'),
-                value: selectedOrder?.salePriceWithTax,
-              },
-            ]
-          : []),
-        {
-          label: t('price-difference'),
-          value: selectedOrder?.priceDifference,
-        },
-        { label: t('paid'), value: selectedOrder?.amountPaid },
-        { label: t('left-to-pay'), value: selectedOrder?.amountLeftToPay },
-        {
-          label: t('expected'),
-          value: selectedOrder?.plannedEndingDate?.toString(),
-        },
-        {
-          label: t('priority'),
-          value: selectedOrder?.priority ? (
-            <Rating
-              readOnly
-              max={3}
-              style={{ color: theme.SECONDARY_1 }}
-              value={orderPriorityArray.indexOf(selectedOrder?.priority) + 1}
-            />
-          ) : (
-            ''
-          ),
-        },
-      ].filter(Boolean) as OrderInfoConfigType[],
-    [
-      t,
-      selectedOrder?.name,
-      selectedOrder?.description,
-      selectedOrder?.note,
-      selectedOrder?.acquisitionCost,
-      selectedOrder?.salePrice,
-      selectedOrder?.legalEntity,
-      selectedOrder?.salePriceWithTax,
-      selectedOrder?.priceDifference,
-      selectedOrder?.amountPaid,
-      selectedOrder?.amountLeftToPay,
-      selectedOrder?.plannedEndingDate,
-      selectedOrder?.priority,
-      authData?.roles,
-    ]
-  );
-
-  const pausingInfoRow = useMemo(() => {
-    if (!isPaused) return null;
-    return isMobile ? (
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <strong className="pause">{t('pausing-comment')}</strong>
-        <span className={classNames('pause', 'pausing-value')}>
-          {selectedOrder?.pausingComment}
-        </span>
-      </div>
-    ) : (
-      <TableRow>
-        <Styled.TableCellContainer component="th" scope="row" className="pause">
-          {t('pausing-comment')}
-        </Styled.TableCellContainer>
-        <Styled.TableCellContainer
-          className={classNames('pause', 'pausing-value')}
-        >
-          {selectedOrder?.pausingComment}
-        </Styled.TableCellContainer>
-      </TableRow>
+    const orderInfoConfig: OrderInfoConfigType[] = useMemo(
+        () =>
+            [
+                { label: t('order-name'), value: selectedOrder?.name },
+                {
+                    label: t('description'),
+                    value: selectedOrder?.description,
+                },
+                { label: t('note'), value: selectedOrder?.note },
+                {
+                    label: t('acquisition-cost'),
+                    value: selectedOrder?.acquisitionCost,
+                },
+                { label: t('sale-price'), value: selectedOrder?.salePrice },
+                ...(selectedOrder?.legalEntity
+                    ? [
+                          {
+                              label: t('sale-price-taxed'),
+                              value: selectedOrder?.salePriceWithTax,
+                          },
+                      ]
+                    : []),
+                {
+                    label: t('price-difference'),
+                    value: selectedOrder?.priceDifference,
+                },
+                { label: t('paid'), value: selectedOrder?.amountPaid },
+                {
+                    label: t('left-to-pay'),
+                    value: selectedOrder?.amountLeftToPay,
+                },
+                {
+                    label: t('expected'),
+                    value: selectedOrder?.plannedEndingDate?.toString(),
+                },
+                {
+                    label: t('priority'),
+                    value: selectedOrder?.priority ? (
+                        <Rating
+                            readOnly
+                            max={3}
+                            style={{ color: theme.SECONDARY_1 }}
+                            value={
+                                orderPriorityArray.indexOf(
+                                    selectedOrder?.priority
+                                ) + 1
+                            }
+                        />
+                    ) : (
+                        ''
+                    ),
+                },
+            ].filter(Boolean) as OrderInfoConfigType[],
+        [
+            t,
+            selectedOrder?.name,
+            selectedOrder?.description,
+            selectedOrder?.note,
+            selectedOrder?.acquisitionCost,
+            selectedOrder?.salePrice,
+            selectedOrder?.legalEntity,
+            selectedOrder?.salePriceWithTax,
+            selectedOrder?.priceDifference,
+            selectedOrder?.amountPaid,
+            selectedOrder?.amountLeftToPay,
+            selectedOrder?.plannedEndingDate,
+            selectedOrder?.priority,
+        ]
     );
-  }, [isMobile, isPaused, selectedOrder?.pausingComment, t]);
 
-  return isMobile ? (
-    <Styled.MobileContainer>
-      {pausingInfoRow}
-      {orderInfoConfig
-        .filter((x) => x.value !== null)
-        .map((info, index) => (
-          <div
-            key={index}
-            style={{ display: 'flex', justifyContent: 'space-between' }}
-          >
-            <strong>{info.label}</strong>
-            <span>{info.value}</span>
-          </div>
-        ))}
-    </Styled.MobileContainer>
-  ) : (
-    <Styled.DesktopContainer>
-      <Table className="order-info-table" aria-label="order info overview">
-        <TableBody>
-          {pausingInfoRow}
-          {orderInfoConfig
-            .filter((x) => x.value !== null)
-            .map((info, index) => (
-              <TableRow key={index}>
+    const pausingInfoRow = useMemo(() => {
+        if (!isPaused) return null;
+        return isMobile ? (
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <strong className="pause">{t('pausing-comment')}</strong>
+                <span className={classNames('pause', 'pausing-value')}>
+                    {selectedOrder?.pausingComment}
+                </span>
+            </div>
+        ) : (
+            <TableRow>
                 <Styled.TableCellContainer
-                  component="th"
-                  scope="row"
-                  style={{ fontWeight: 'bold' }}
+                    component="th"
+                    scope="row"
+                    className="pause"
                 >
-                  {info.label}
+                    {t('pausing-comment')}
                 </Styled.TableCellContainer>
-                <Styled.TableCellContainer className="value">
-                  {info.value}
+                <Styled.TableCellContainer
+                    className={classNames('pause', 'pausing-value')}
+                >
+                    {selectedOrder?.pausingComment}
                 </Styled.TableCellContainer>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
-    </Styled.DesktopContainer>
-  );
+            </TableRow>
+        );
+    }, [isMobile, isPaused, selectedOrder?.pausingComment, t]);
+
+    return isMobile ? (
+        <Styled.MobileContainer>
+            {pausingInfoRow}
+            {orderInfoConfig
+                .filter((x) => x.value !== null)
+                .map((info, index) => (
+                    <div
+                        key={index}
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                        }}
+                    >
+                        <strong>{info.label}</strong>
+                        <span>{info.value}</span>
+                    </div>
+                ))}
+        </Styled.MobileContainer>
+    ) : (
+        <Styled.DesktopContainer>
+            <Table
+                className="order-info-table"
+                aria-label="order info overview"
+            >
+                <TableBody>
+                    {pausingInfoRow}
+                    {orderInfoConfig
+                        .filter((x) => x.value !== null)
+                        .map((info, index) => (
+                            <TableRow key={index}>
+                                <Styled.TableCellContainer
+                                    component="th"
+                                    scope="row"
+                                    style={{ fontWeight: 'bold' }}
+                                >
+                                    {info.label}
+                                </Styled.TableCellContainer>
+                                <Styled.TableCellContainer className="value">
+                                    {info.value}
+                                </Styled.TableCellContainer>
+                            </TableRow>
+                        ))}
+                </TableBody>
+            </Table>
+        </Styled.DesktopContainer>
+    );
 };
 
 export default OrderInfoOverview;
